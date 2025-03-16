2db115c67fab329c746337544f2ece2a7bf2f386
Victor <victoremmadrid@outlook.com>
2025-03-16 17:36:40 +0100

    Correção pasta docs para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -9,15 +9,16 @@ import vue from "@vitejs/plugin-vue";
 // Línea 4: Exportamos por defecto la configuración de Vite utilizando la función "defineConfig".
 // Esta estructura nos permite organizar de forma clara todas las opciones de configuración necesarias para el proyecto.
 export default defineConfig({
+  // Línea 10: Definimos la propiedad "plugins", que es un array donde registramos los plugins que Vite usará.
+  // En este caso, estamos utilizando el plugin de Vue, invocándolo con "vue()".
+  // Esto permite que los archivos .vue sean procesados correctamente por Vite.
+  plugins: [vue()],
+
   // Línea 6: Definimos la propiedad "base", que especifica la URL base de la aplicación cuando se despliega.
   // Este ajuste es importante cuando el proyecto se aloja en un subdirectorio, como en GitHub Pages.
   // Aquí "/portafolio-vue/" representa la ruta en la que estará accesible el sitio.
   base: "/portafolio-vue/",
 
-  // Línea 10: Definimos la propiedad "plugins", que es un array donde registramos los plugins que Vite usará.
-  // En este caso, estamos utilizando el plugin de Vue, invocándolo con "vue()".
-  // Esto permite que los archivos .vue sean procesados correctamente por Vite.
-  plugins: [vue()],
 
   // Línea 14: Agregamos la configuración "build" para definir opciones relacionadas con la construcción (compilación) del proyecto.
   build: {
