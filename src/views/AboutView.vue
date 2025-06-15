c43f9d70544b48e9d5404203b562161d07569a9d
Victor <victoremmadrid@outlook.com>
2025-06-15 20:47:52 +0200

    Actualización del archivo Aboutview

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container text-center py-5">
-    <h1 class="display-5">Sobre Mim</h1>
+    <h1 class="display-5">Sobre mí</h1>
     <p class="lead">
-      Sou um desenvolvedor web apaixonado por tecnologia e inovação.
+      Soy un desarrollador web apasionado por la tecnología y la innovación.
     </p>
     <img
       src="https://via.placeholder.com/200"
